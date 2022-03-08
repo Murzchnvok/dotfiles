206edbaf1beb265ddf6eab58a4b986ecba689cdc
Murzchnvok <murzchnvok@gmail.com>
2022-03-08 18:36:58 -0300

    updated

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -1,32 +1,35 @@
-vim.cmd([[
-filetype plugin on
-syntax on
-if has('termguicolors')
-  set termguicolors
-endif
-
-set background=dark
-let g:gruvbox_material_palette = 'mix'
-let g:gruvbox_material_enable_bold = 1
-let g:gruvbox_material_diagnostic_line_highlight = 1
-let g:gruvbox_material_statusline_style = 'mix'
-colorscheme gruvbox-material
-
-let g:nvim_tree_indent_markers = 1
-let g:nvim_tree_show_icons = { 'git': 1, 'folders': 0, 'files': 1, 'folder_arrows': 0 }
-highlight NvimTreeSymlink guifg=#83a598
-highlight NvimTreeFolderName guifg=#a89984 gui=bold
-highlight NvimTreeEmptyFolderName guifg=#a89984
-highlight NvimTreeOpenedFolderName guifg=#8ec07c gui=bold
-highlight NvimTreeExecFile guifg=#b8bb26 gui=bold
-highlight NvimTreeIndentMarker guifg=#8ec07c
-highlight NvimTreeStatusLine guibg=#00000000
-
-let g:neoformat_enabled_python = [ 'black', 'isort' ]
-let g:neoformat_run_all_formatters = 1
-]])
-
 local set = vim.opt
+local g = vim.g
+local cmd = vim.api.nvim_command
+local eval = vim.api.nvim_eval
+
+cmd('filetype plugin on')
+cmd('syntax on')
+
+if vim.fn.has('termguicolors') then
+  set.termguicolors = true 
+end
+
+set.background = 'dark'
+g['gruvbox_material_palette'] = 'mix'
+g['gruvbox_material_enable_bold'] = 1
+g['gruvbox_material_diagnostic_line_highlight'] = 0
+g['gruvbox_material_statusline_style'] = 'mix'
+cmd('colorscheme gruvbox-material')
+
+g['nvim_tree_indent_markers'] = 1
+g['nvim_tree_show_icons'] = eval('{ "git": 1, "folders": 0, "files": 1, "folder_arrows": 0 }')
+
+g['neoformat_enable_python'] = eval('[ "black", "isort" ]')
+g['neoformat_run_all_formatters'] = 1
+
+cmd('highlight NvimTreeSymlink guifg=#83a598')
+cmd('highlight NvimTreeFolderName guifg=#a89984 gui=bold')
+cmd('highlight NvimTreeEmptyFolderName guifg=#a89984')
+cmd('highlight NvimTreeOpenedFolderName guifg=#8ec07c gui=bold')
+cmd('highlight NvimTreeExecFile guifg=#b8bb26 gui=bold')
+cmd('highlight NvimTreeIndentMarker guifg=#8ec07c')
+cmd('highlight NvimTreeStatusLine guibg=#00000000')
 
 set.showmode = false
 
