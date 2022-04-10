50425fe1b5b990f84f516bc746a0d723772f7872
Murzchnvok <murzchnvok@gmail.com>
2022-04-10 20:56:30 -0300

    updated

--- nvim/lua/plugins/config/nvim-tree.lua
+++ nvim/lua/plugins/config/nvim-tree.lua
@@ -1,5 +1,40 @@
 require("nvim-tree").setup({
+	actions = {
+		open_file = {
+			resize_window = true,
+		},
+	},
+
+	diagnostics = {
+		enable = true,
+		show_on_dirs = true,
+		icons = {
+			error = "E",
+			warning = "W",
+			info = "I",
+			hint = "H",
+		},
+	},
+
+	renderer = {
+		indent_markers = {
+			enable = true,
+			icons = {
+				corner = "└ ",
+				edge = "│ ",
+				none = "  ",
+			},
+		},
+	},
+
+	update_focused_file = {
+		enable = false,
+		update_cwd = false,
+		ignore_list = {},
+	},
+
 	view = {
 		hide_root_folder = true,
+		signcolumn = "yes",
 	},
 })
--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -1,7 +1,6 @@
 local set = vim.opt
 local g = vim.g
 local cmd = vim.api.nvim_command
-local eval = vim.api.nvim_eval
 
 cmd("language en_US")
 cmd("filetype plugin on")
@@ -12,24 +11,36 @@ if vim.fn.has("termguicolors") then
 end
 
 set.background = "dark"
-g["gruvbox_material_palette"] = "mix"
-g["gruvbox_material_statusline_style"] = "mix"
-g["gruvbox_material_enable_italic"] = 1
-g["gruvbox_material_diagnostic_text_highlight"] = 1
-g["gruvbox_material_diagnostic_line_highlight"] = 1
-g["gruvbox_material_diagnostic_virtual_text"] = "colored"
-g["gruvbox_material_sign_column_background"] = "none"
+g.gruvbox_material_palette = "mix"
+g.gruvbox_material_statusline_style = "mix"
+g.gruvbox_material_enable_italic = 1
+g.gruvbox_material_diagnostic_text_highlight = 1
+g.gruvbox_material_diagnostic_line_highlight = 1
+g.gruvbox_material_diagnostic_virtual_text = "colored"
+g.gruvbox_material_sign_column_background = "none"
 cmd("colorscheme gruvbox-material")
 
-g["nvim_tree_indent_markers"] = 1
-g["nvim_tree_show_icons"] = eval('{ "git": 1, "folders": 0, "files": 1, "folder_arrows": 0 }')
-cmd("hi NvimTreeSymlink guifg=#83a598")
-cmd("hi NvimTreeFolderName guifg=#a89984 gui=bold")
+g.nvim_tree_show_icons = {
+	git = 1,
+	folders = 1,
+	files = 1,
+	folder_arrows = 1,
+}
+
 cmd("hi NvimTreeEmptyFolderName guifg=#a89984")
-cmd("hi NvimTreeOpenedFolderName guifg=#8ec07c gui=bold")
-cmd("hi NvimTreeExecFile guifg=#b8bb26 gui=bold")
-cmd("hi NvimTreeIndentMarker guifg=#8ec07c")
-cmd("hi NvimTreeStatusLine guibg=#00000000")
+cmd("hi NvimTreeFolderIcon guifg=#a89984")
+cmd("hi NvimTreeFolderName guifg=#a89984 gui=bold")
+cmd("hi NvimTreeOpenedFolderName guifg=#e2cca9 gui=bold")
+
+cmd("hi NvimTreeExecFile guifg=#b0b846 gui=bold")
+cmd("hi NvimTreeSymlink guifg=#80aa9e gui=bold")
+
+cmd("hi NvimTreeIndentMarker guifg=#a89984")
+
+cmd("hi NvimTreeLspDiagnosticsError guifg=#f2594b")
+cmd("hi NvimTreeLspDiagnosticsWarning guifg=#e9b143")
+cmd("hi NvimTreeLspDiagnosticsInfo guifg=#80aa9e")
+cmd("hi NvimTreeLspDiagnosticsHint guifg=#b0b846")
 
 cmd("hi StatusLine guibg=none")
 cmd("hi StatusLineNC guibg=none guifg=none")
